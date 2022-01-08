15907e45b5a482a5cf75515a1e063c84a36501c8
Juanjo Lainez <juanjo.reche@creditorwatch.com.au>
2022-01-08 12:08:18 +1100

    Update version

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,26 +1,25 @@
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-const Web3 = require("web3");
-const { interface, bytecode } = require("./compile");
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
 
-const provider = new HDWalletProvider(
-  "Add 12 words mnemonic",
-  "Add Infura API auth from https://infura.io/dashboard"
+const { abi, evm } = require('./compile');
+
+provider = new HDWalletProvider(
+  'REPLACE_WITH_YOUR_MNEMONIC',
+  'REPLACE_WITH_YOUR_INFURA_URL'
 );
 
-const webInstance = new Web3(provider);
+const web3 = new Web3(provider);
 
 const deploy = async () => {
-  const accounts = await webInstance.eth.getAccounts();
-  console.log("Attempting to deploy from account " + accounts[0]);
+  const accounts = await web3.eth.getAccounts();
 
-  const result = await new webInstance.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hello there'] })
-    .send({ from: accounts[0], gas: "1000000" });
+  console.log('Attempting to deploy from account', accounts[0]);
 
-  console.log(result.options.address);
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
 
+  console.log('Contract deployed to', result.options.address);
   provider.engine.stop();
-
 };
-
 deploy();
